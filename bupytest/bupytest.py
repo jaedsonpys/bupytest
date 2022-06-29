930636f07da421cd232d4b0f32aa2a191bf056db
Jaedson Silva <contacanal798@gmail.com>
2022-06-29 18:24:59 -0300

    adding UnitTest.run() method to run all tests

--- bupytest/bupytest.py
+++ bupytest/bupytest.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+
 class UnitTest:
     def __init__(self):
-        self.test_methods = []
+        self._test_methods = []
+        self._finished_tests = {}
         self.failed_test = {}
 
     def _get_test_methods(self) -> list:
@@ -11,6 +15,19 @@ class UnitTest:
                 test_methods.append(attr_obj)
 
         return test_methods
+
+    def run(self):
+        self._test_methods = self._get_test_methods()
+        for test in self._test_methods:
+            method_name = test.__name__
+            start_time = datetime.now()
+            test.__call__()
+            finished_time = datetime.now()
+
+            self._finished_tests[method_name] = {
+                'start': start_time,
+                'finish': finished_time
+            }
 
 
 if __name__ == '__main__':
