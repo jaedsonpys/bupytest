9098f5f4e6fc77acf986c1503246515f585bbbc9
Jaedson Silva <contacanal798@gmail.com>
2022-06-29 18:17:13 -0300

    returning "test_methods" list in UnitTest._get_test_methods()

--- bupytest/bupytest.py
+++ bupytest/bupytest.py
@@ -3,11 +3,14 @@ class UnitTest:
         self.test_methods = []
         self.failed_test = {}
 
-    def get_test_methods(self):
+    def _get_test_methods(self) -> list:
+        test_methods = []
         for attr in self.__dir__():
             attr_obj = self.__getattribute__(attr)
             if attr.startswith('test_') and callable(attr_obj):
-                self.test_methods.append(attr_obj)
+                test_methods.append(attr_obj)
+
+        return test_methods
 
 
 if __name__ == '__main__':
