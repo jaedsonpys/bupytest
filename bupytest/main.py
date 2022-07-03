8936e14e897725661c22130482cd7d662be51ac1
Jaedson Silva <contacanal798@gmail.com>
2022-07-03 15:44:10 -0300

    handler "test -d" argument to execute a test module

--- bupytest/main.py
+++ bupytest/main.py
@@ -1,4 +1,4 @@
-from .execution import execute_module, execute_modules_dir
+from . import execution
 from argeasy import ArgEasy
 
 import os
@@ -14,3 +14,16 @@ def main():
     parser.add_argument('test', 'Tests a module or a module directory', action='store_true')
     parser.add_flag('-d', 'Specifies that the path of "test" is a directory')
     parser.add_flag('-m', 'Specifies that the path of "test" is a module')
+
+    args = parser.get_args()
+
+    if args.test:
+        file = args.m
+        directory = args.d
+
+        if file and os.path.isfile(file):
+            file = file.replace('.py', '')
+            execution.execute_module(file)
+        else:
+            print(f'\033[31merror: file "{file}" not found\033[m')
+            return True
