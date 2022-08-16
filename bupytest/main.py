dd702e22e168de37f89d5e32cc024ad5afe2d83e
Jaedson Silva <contacanal798@gmail.com>
2022-08-16 12:59:35 -0300

    removing "-d" and "-m" flags from parser

--- bupytest/main.py
+++ bupytest/main.py
@@ -28,31 +28,22 @@ def main():
         version='0.3.1'
     )
 
-    parser.add_argument('test', 'Tests a module or a module directory', action='store_true')
-    parser.add_flag('-d', 'Specifies that the path of "test" is a directory')
-    parser.add_flag('-m', 'Specifies that the path of "test" is a module')
+    parser.add_argument('test', 'Tests a module or a module directory')
 
     args = parser.get_args()
-    result = False
+    result = 0
 
     if args.test:
         sys.path.insert(0, './')
+        filepath = args.test
 
-        file = args.m
-        directory = args.d
-
-        if file:
-            if os.path.isfile(file):
-                file = file.replace('.py', '')
-                result = execution.execute_module(file)
-            else:
-                print(f'\033[31merror: file "{file}" not found\033[m')
-                return True
-        elif directory:
-            if os.path.isdir(directory):
-                result = execution.execute_modules_dir(directory)
-            else:
-                print(f'\033[31merror: directory "{directory}" not found\033[m')
-                return True
+        if os.path.isfile(filepath):
+            file = filepath.replace('.py', '')
+            result = execution.execute_module(file)
+        elif os.path.isdir(filepath):
+            result = execution.execute_modules_dir(filepath)
+        else:
+            print(f'\033[31merror: file or directory "{filepath}" not found\033[m')
+            return 1
 
     return result
