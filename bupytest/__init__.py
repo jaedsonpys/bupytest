528fc4d91fb0d31188c8fcb6aa9845773dffc0cf
Jaedson Silva <contacanal798@gmail.com>
2023-02-04 12:49:45 -0300

    feat: update license copyright year

--- bupytest/__init__.py
+++ bupytest/__init__.py
@@ -1,5 +1,5 @@
 # BuPyTest
-# Copyright (C) 2022  Jaedson Silva
+# Copyright (C) 2023  Jaedson Silva
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
